5905f99c76bf2fe7f2ea4f40cecd84555fb0995e
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-06-10 12:43:59 +0100

    Utils Notifications criado e feito

---
+++ src/utils/notifications.js
@@ -0,0 +1,17 @@
+import toast from "react-hot-toast";
+
+export const notifySuccess = (message) => {
+  toast.success(message);
+};
+
+export const notifyError = (message) => {
+  toast.error(message);
+};
+
+export const notifyLoading = (message) => {
+  return toast.loading(message);
+};
+
+export const dismissToast = (toastId) => {
+  toast.dismiss(toastId);
+};
